9dbb56e1e4a86a393832b413ab52614dc2f0d3a5
Oz Tamir <oz@blockaid.co>
2022-12-28 18:02:21 +0200

    Moved the prompt to the cloud

--- src/HowTheyMet.js
+++ src/HowTheyMet.js
@@ -3,13 +3,11 @@ import './App.css';
 import { Typography } from '@mui/material';
 
 function HowTheyMet() {
-  const API_ENDPOINT = 'https://this-dude-guy-mentioned-does-not-exist.oztamir.workers.dev/?prompt=';
-  const NAMES = ['Gal', 'Yuval', 'Adi', 'Noam', 'Eden', 'Amit', 'Danial', 'Lior', 'Or', 'Netta'];
-  const [name, ] = useState(NAMES[Math.floor(Math.random() * NAMES.length)]);
+  const API_ENDPOINT = 'https://this-dude-guy-mentioned-does-not-exist.oztamir.workers.dev/';
   const [data, setData] = useState("Loading...");
  
   useEffect(() => {
-   fetch(API_ENDPOINT + name)
+   fetch(API_ENDPOINT)
     .then(async (response) => {
         let json = await response.json();
         setData(json.choices[0].text);
@@ -17,7 +15,7 @@ function HowTheyMet() {
     .catch((error) => {
         setData("We can't tell how they met.");
     })
-  }, [name]);
+  }, []);
 
   return (
     <Typography variant="h5" component="h5" className="How-They-Met">
