76fa023a820ae021e31f6886c1441a4c4323d3fe
Oz Tamir <oz@blockaid.co>
2022-12-28 17:37:43 +0200

    Added favicon

--- src/HowTheyMet.js
+++ src/HowTheyMet.js
@@ -1,25 +1,27 @@
-import useAxios from 'axios-hooks';
+import { useState, useEffect } from "react";
 import './App.css';
 import { Typography } from '@mui/material';
 
 function HowTheyMet() {
   const API_ENDPOINT = 'https://this-dude-guy-mentioned-does-not-exist.oztamir.workers.dev/?prompt=';
-  const NAMES = ['Gal', 'Yuval', 'Adi', 'Noam', 'Eden', 'Amit', 'Danial', 'Lior', 'Osher', 'Or', 'Netta'];
-  const randomName = NAMES[Math.floor(Math.random() * NAMES.length)];
-
-  const [{ data, loading, error }, refetch] = useAxios({
-    url: API_ENDPOINT + randomName,
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  })
-
-  if (loading) return <p>Loading...</p>
-  if (error) return <p>We can't tell how they met.</p>
+  const NAMES = ['Gal', 'Yuval', 'Adi', 'Noam', 'Eden', 'Amit', 'Danial', 'Lior', 'Or', 'Netta'];
+  const [name, ] = useState(NAMES[Math.floor(Math.random() * NAMES.length)]);
+  const [data, setData] = useState("Loading...");
+ 
+  useEffect(() => {
+   fetch(API_ENDPOINT + name)
+    .then(async (response) => {
+        let json = await response.json();
+        setData(json.choices[0].text);
+    })
+    .catch((error) => {
+        setData("We can't tell how they met.");
+    })
+  }, [name]);
 
   return (
     <Typography variant="h5" component="h5">
-      {data.choices[0].text}
+      {data}
     </Typography>
   );
 }
