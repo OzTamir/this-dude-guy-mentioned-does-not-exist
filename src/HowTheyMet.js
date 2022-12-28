dd56d137c60b298a372e31fbd5f88da965f88841
Oz Tamir <oz@blockaid.co>
2022-12-28 17:09:02 +0200

    Added names

--- src/HowTheyMet.js
+++ src/HowTheyMet.js
@@ -4,18 +4,21 @@ import { Typography } from '@mui/material';
 
 function HowTheyMet() {
   const API_ENDPOINT = 'https://this-dude-guy-mentioned-does-not-exist.oztamir.workers.dev/?prompt=';
-  const NAMES = ['Gal', 'Yuval', 'Adi', 'Noam', 'Eden', 'Amit'];
+  const NAMES = ['Gal', 'Yuval', 'Adi', 'Noam', 'Eden', 'Amit', 'Danial', 'Lior', 'Osher', 'Or', 'Netta'];
   const randomName = NAMES[Math.floor(Math.random() * NAMES.length)];
 
-  const [{ data, loading, error }, refetch] = useAxios(
-    API_ENDPOINT + randomName
-  )
+  const [{ data, loading, error }, refetch] = useAxios({
+    url: API_ENDPOINT + randomName,
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
 
   if (loading) return <p>Loading...</p>
   if (error) return <p>We can't tell how they met.</p>
 
   return (
-    <Typography variant="h2" component="h2">
+    <Typography variant="h5" component="h5">
       {data.choices[0].text}
     </Typography>
   );
